5cdf09dcb1654c566b61558ae01ec2baae825c39
Nipun Kulshrestha <nipz.kul@gmail.com>
2015-11-20 14:50:05 +0530

    Main Program
    Main Program for Assembler.
    the included header file "assemble.h" will convert the file to its
    object file.

---
+++ Assembler.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include"assemble.h"
+
+void main()
+{
+
+    char ProgramFile[50];
+    printf("\n Enter the Programs File Name:");
+    gets(ProgramFile);
+    PassOne(ProgramFile);
+
+}
